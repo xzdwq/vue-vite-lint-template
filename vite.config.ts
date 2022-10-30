8e1824e0c9c359e82dc1655817527e82cf527e11
xzdwq <vazhivodrov@mail.ru>
2022-10-30 23:15:32 +0300

    del plugin vue-i18n

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { quasar, transformAssetUrls } from '@quasar/vite-plugin';
 import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
-import VueI18n from '@intlify/vite-plugin-vue-i18n';
 import { defineConfig } from 'vite';
 import path from 'node:path';
 import vue from '@vitejs/plugin-vue';
@@ -42,11 +41,6 @@ export default defineConfig({
       dirs: ['src/**/*'],
       resolvers: [ElementPlusResolver()],
       vueTemplate: true,
-    }),
-    VueI18n({
-      runtimeOnly: true,
-      compositionOnly: true,
-      include: [path.resolve(__dirname, 'locales/**')],
     }),
     yaml({
       transform(data) {
